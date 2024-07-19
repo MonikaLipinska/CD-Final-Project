75729fbea734e77436ffeacaf1a574b2e26efa0d
MonikaLipinska <monika.l.lipinska@gmail.com>
2024-07-19 18:31:04 +0200

    root pop

--- final-project/src/index.jsx
+++ final-project/src/index.jsx
@@ -5,18 +5,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import './styles/App.scss'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 
 /*
 ReactDOM.createRoot(document.getElementById('root')).render(
  */
-
+root.render (
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-    document.getElementById('root')
+
 );
 
