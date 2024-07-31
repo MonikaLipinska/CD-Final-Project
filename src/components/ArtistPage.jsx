4d675d9fd3f0c65ef7a33433c8e9357e5a89e836
MonikaLipinska <monika.l.lipinska@gmail.com>
2024-07-31 18:11:35 +0200

    art

--- src/components/ArtistPage.jsx
+++ src/components/ArtistPage.jsx
@@ -4,6 +4,11 @@ import styles from "@/styles/ArtistPage.module.scss";
 import monroe from "@/photo/monroe.jpeg";
 import AE2 from "@/photo/AE2.png";
 import dali from "@/photo/dali.jpeg";
+import obraz1 from "@/photo/obraz1.jpg";
+import obraz2 from "@/photo/obraz2.jpg";
+import obraz3 from "@/photo/obraz3.jpg";
+import obraz9 from "@/photo/obraz9.jpeg";
+import obraz8 from "@/photo/obraz8.jpeg";
 
 
 const artists = [
@@ -11,19 +16,22 @@ const artists = [
         id: 1,
         name: "One",
         image: AE2,
-        description: "Opis/Description"
+        description: "Opis/Description",
+        thumbnails: [obraz9, obraz1, obraz2, obraz3, obraz8]
     },
     {
         id: 2,
         name: "Two",
         image: monroe,
-        description: "Opis/Description"
+        description: "Opis/Description",
+        thumbnails: [obraz1, obraz2, obraz3]
     },
     {
         id: 3,
         name: "Three",
         image: dali,
-        description: "Opis/Description"
+        description: "Opis/Description",
+        thumbnails: [obraz1, obraz2, obraz3]
     }
 ];
 
@@ -38,15 +46,8 @@ const ArtistPage = () => {
         return <div>Artist not found</div>;
     }
 
-
-    {/*const handleArtistChange = (e) => {
-        const selectedId = e.target.value;
-        navigate(`/browse/artist/${selectedId}`);
-    };*/
-    }
-
     return (
-        <div className={styles.ArtistPage}>
+        <div className={styles.artistPage}>
             {/*<select value={id} onChange={handleArtistChange}>
                 {artists.map(artist => (
                     <option key={artist.id} value={artist.id}>{artist.name}</option>
@@ -55,6 +56,14 @@ const ArtistPage = () => {
             <h2 className={styles.artistTitle}>{artist.name}</h2>
             <img className={styles.artistImage} src={artist.image} alt={artist.name}/>
             <p className={styles.artistDescription}>{artist.description}</p>
+
+            <div className={styles.thumbnailContainer}>
+                {artist.thumbnails.map((thumbnail, index) => (
+                    <img key={index} className={styles.thumbnail} src={thumbnail}
+                         alt={`${artist.name} thumbnail ${index + 1}`}/>
+                ))}
+            </div>
+
         </div>
     );
 };
